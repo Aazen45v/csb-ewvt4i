cf2df95fe706bc7ee5cca85535c27c56a5d1cb61
Code Sandbox <hello@codesandbox.io>
2023-02-25 19:04:23 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8e8c598250e8c480c341bd947f9ca696.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1018cc7302efca8f7c1e5c533d12ff9",
+    "url": "https://aazen45v.github.io/csb-ewvt4i/index.html"
+  },
+  {
+    "revision": "f51d9f3117eb32b16031",
+    "url": "https://aazen45v.github.io/csb-ewvt4i/static/css/main.d495e4f2.chunk.css"
+  },
+  {
+    "revision": "7dad2ba48f407990deea",
+    "url": "https://aazen45v.github.io/csb-ewvt4i/static/js/2.97eb0e39.chunk.js"
+  },
+  {
+    "revision": "f51d9f3117eb32b16031",
+    "url": "https://aazen45v.github.io/csb-ewvt4i/static/js/main.3d1a1577.chunk.js"
+  },
+  {
+    "revision": "fc689873ef86a1cba5f4",
+    "url": "https://aazen45v.github.io/csb-ewvt4i/static/js/runtime~main.970eda7e.js"
+  }
+]);
